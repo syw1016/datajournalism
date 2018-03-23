f1346dade4c0ec355b5b5b8b381103d57f35fe29
syw1016 <ysung1016@gmail.com>
2018-03-22 18:21:59 -0700

    basic done

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,3 @@
-// Step 0: Set up our chart
-//= ================================
 var svgWidth = 960;
 var svgHeight = 500;
 
@@ -8,7 +6,6 @@ var margin = { top: 20, right: 40, bottom: 80, left: 100 };
 var width = svgWidth - margin.left - margin.right;
 var height = svgHeight - margin.top - margin.bottom;
 
-// Create an SVG wrapper, append an SVG group that will hold our chart, and shift the latter by left and top margins.
 var svg = d3
   .select(".chart")
   .append("svg")
@@ -17,7 +14,6 @@ var svg = d3
   .append("g")
   .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-// Append an SVG group
 var chart = svg.append("g");
 
 // Append a div to the bodyj to create tooltips, assign it a class
@@ -27,204 +23,240 @@ d3.select(".chart").append("div").attr("class", "tooltip").style("opacity", 0);
 d3.csv("data.csv", function(err, data) {
   if (err) throw err;
 
-  data.forEach(function(data) {
+  data.forEach(row => {
+    row.dental_visit = +row.dental_visit;
+    row.self_excellent = +row.self_excellent;
+    row.self_poor = +row.self_poor;
 
+    row.poverty = +row.poverty;
+    row.median_income = +row.median_income;
   });
 
-  // Create scale functions
   var yLinearScale = d3.scaleLinear().range([height, 0]);
-
   var xLinearScale = d3.scaleLinear().range([0, width]);
 
-  // Create axis functions
   var bottomAxis = d3.axisBottom(xLinearScale);
   var leftAxis = d3.axisLeft(yLinearScale);
 
-  // These variables store the minimum and maximum values in a column in data.csv
-  var xMin;
-  var xMax;
-  var yMax;
-
-  // This function identifies the minimum and maximum values in a column in hairData.csv
-  // and assign them to xMin and xMax variables, which will define the axis domain
-  function findMinAndMax(dataColumnX) {
-    xMin = d3.min(data, function(data) {
-      return +data[dataColumnX] * 0.8;
+  function findMinAndMax(dataColumn) {
+    min = d3.min(data, function(data) {
+      return +data[dataColumn] * 0.8;
     });
 
-    xMax = d3.max(data, function(data) {
-      return +data[dataColumnX] * 1.1;
+    max = d3.max(data, function(data) {
+      return +data[dataColumn] * 1.1;
     });
 
-    yMax = d3.max(data, function(data) {
-      return +data.num_hits * 1.1;
-    });
+    return {min, max}
   }
 
-  // The default x-axis is 'hair_length'
-  // Another axis can be assigned to the variable during an onclick event.
-  // This variable is key to the ability to change axis/data column
-  var currentAxisLabelX = "hair_length";
+  var currentAxisLabelX = "poverty";
+  var currentAxisLabelY = "dental_visit";
 
-  // Call findMinAndMax() with 'hair_length' as default
-  findMinAndMax(currentAxisLabelX);
+  var x = findMinAndMax(currentAxisLabelX);
+  var y = findMinAndMax(currentAxisLabelY);
+  xLinearScale.domain([x.min, x.max]);
+  yLinearScale.domain([y.min, y.max]);
 
-  // Set the domain of an axis to extend from the min to the max value of the data column
-  xLinearScale.domain([xMin, xMax]);
-  yLinearScale.domain([0, yMax]);
-
-  // Initialize tooltip
   var toolTip = d3
-    .tip()
-    .attr("class", "tooltip")
-    // Define position
-    .offset([80, -60])
-    // The html() method allows us to mix JavaScript with HTML in the callback function
-    .html(function(data) {
-      var bandName = data.rockband;
-      var numHits = +data.num_hits;
-      var bandInfo = +data[currentAxisLabelX];
-      var bandString;
-      // Tooltip text depends on which axis is active/has been clicked
-      if (currentAxisLabelX === "hair_length") {
-        bandString = "Hair length: ";
-      }
-      else {
-        bandString = "Number of albums: ";
-      }
-      return bandName +
-        "<br>" +
-        bandString +
-        bandInfo +
-        "<br> Hits: " +
-        numHits;
-    });
+      .tip()
+      .attr("class", "tooltip")
+      .offset([80, -60])
+      .html(function(data) {
+        return data.state
+      });
 
-  // Create tooltip
   chart.call(toolTip);
 
-  chart
+    chart
     .selectAll("circle")
-    .data(hairData)
+    .data(data)
     .enter()
     .append("circle")
     .attr("cx", function(data, index) {
-      return xLinearScale(+data[currentAxisLabelX]);
+      return xLinearScale(data[currentAxisLabelX]);
     })
     .attr("cy", function(data, index) {
-      return yLinearScale(data.num_hits);
+      return yLinearScale(data[currentAxisLabelY]);
     })
     .attr("r", "15")
-    .attr("fill", "#E75480")
-    // display tooltip on click
+    .attr("fill", "#87AFC7")
     .on("mouseover", function(data) {
       toolTip.show(data);
     })
-    // hide tooltip on mouseout
     .on("mouseout", function(data, index) {
       toolTip.hide(data);
     });
 
-  // Append an SVG group for the x-axis, then display the x-axis
-  chart
+    chart.selectAll("text")
+      .data(data)
+      .enter()
+      .append("text")
+      .text(function (data) {
+          return data.state_abbr;
+      })
+      .attr("x", function (data) {
+          return xLinearScale(data[currentAxisLabelX]);
+      })
+      .attr("y", function (data) {
+          return yLinearScale(data[currentAxisLabelY]);
+      })
+      .attr("text-anchor", "middle")
+      .attr("class","state-abbr")
+      .on("mouseover", function(data) {
+        toolTip.show(data);
+      })
+      .on("mouseout", function(data, index) {
+        toolTip.hide(data);
+      });
+
+    chart
     .append("g")
     .attr("transform", "translate(0," + height + ")")
-    // The class name assigned here will be used for transition effects
     .attr("class", "x-axis")
     .call(bottomAxis);
 
-  // Append a group for y-axis, then display it
-  chart.append("g").call(leftAxis);
+    chart.append("g").call(leftAxis);
 
-  // Append y-axis label
-  chart
+    chart
+    .append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("y", 0 - margin.left + 20)
+    .attr("x", 0 - height / 2)
+    .attr("dy", "1em")
+    .attr("class", "y-axis-text active")
+    .attr("data-axis-name", "dental_visit")
+    .text("Visited the dentist within the past year (%)");
+
+    chart
     .append("text")
     .attr("transform", "rotate(-90)")
     .attr("y", 0 - margin.left + 40)
     .attr("x", 0 - height / 2)
     .attr("dy", "1em")
-    .attr("class", "axis-text")
-    .attr("data-axis-name", "num_hits")
-    .text("Number of Billboard 500 Hits");
+    .attr("class", "y-axis-text inactive")
+    .attr("data-axis-name", "self_excellent")
+    .text("People self-evaluated their general health as Excellent (%)");
 
-  // Append x-axis labels
-  chart
+    chart
+    .append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("y", 0 - margin.left + 60)
+    .attr("x", 0 - height / 2)
+    .attr("dy", "1em")
+    .attr("class", "y-axis-text inactive")
+    .attr("data-axis-name", "self_poor")
+    .text("People self-evaluated their general health as Poor (%)");
+
+    chart
     .append("text")
     .attr(
       "transform",
       "translate(" + width / 2 + " ," + (height + margin.top + 20) + ")"
     )
-    // This axis label is active by default
-    .attr("class", "axis-text active")
-    .attr("data-axis-name", "hair_length")
-    .text("Hair Metal Band Hair Length (inches)");
+    .attr("class", "x-axis-text active")
+    .attr("data-axis-name", "poverty")
+    .text("Poverty (%)");
 
-  chart
+    chart
     .append("text")
     .attr(
       "transform",
       "translate(" + width / 2 + " ," + (height + margin.top + 45) + ")"
     )
-    // This axis label is inactive by default
-    .attr("class", "axis-text inactive")
-    .attr("data-axis-name", "num_albums")
-    .text("# of Albums Released");
+    .attr("class", "x-axis-text inactive")
+    .attr("data-axis-name", "median_income")
+    .text("Median Income ($)");
 
-  // Change an axis's status from inactive to active when clicked (if it was inactive)
-  // Change the status of all active axes to inactive otherwise
-  function labelChange(clickedAxis) {
+  function XlabelChange(clickedAxis) {
     d3
-      .selectAll(".axis-text")
+      .selectAll(".x-axis-text")
       .filter(".active")
-      // An alternative to .attr("class", <className>) method. Used to toggle classes.
       .classed("active", false)
       .classed("inactive", true);
-
     clickedAxis.classed("inactive", false).classed("active", true);
   }
 
-  d3.selectAll(".axis-text").on("click", function() {
-    // Assign a variable to current axis
-    var clickedSelection = d3.select(this);
-    // "true" or "false" based on whether the axis is currently selected
-    var isClickedSelectionInactive = clickedSelection.classed("inactive");
-    // console.log("this axis is inactive", isClickedSelectionInactive)
-    // Grab the data-attribute of the axis and assign it to a variable
-    // e.g. if data-axis-name is "poverty," var clickedAxis = "poverty"
-    var clickedAxis = clickedSelection.attr("data-axis-name");
-    console.log("current axis: ", clickedAxis);
+  function YlabelChange(clickedAxis) {
+    d3
+      .selectAll(".y-axis-text")
+      .filter(".active")
+      .classed("active", false)
+      .classed("inactive", true);
+    clickedAxis.classed("inactive", false).classed("active", true);
+  }
 
-    // The onclick events below take place only if the x-axis is inactive
-    // Clicking on an already active axis will therefore do nothing
+  d3.selectAll(".x-axis-text").on("click", function() {
+    var clickedSelection = d3.select(this);
+    var isClickedSelectionInactive = clickedSelection.classed("inactive");
+    var clickedAxis = d3.select(this).attr("data-axis-name");
+
     if (isClickedSelectionInactive) {
-      // Assign the clicked axis to the variable currentAxisLabelX
       currentAxisLabelX = clickedAxis;
-      // Call findMinAndMax() to define the min and max domain values.
-      findMinAndMax(currentAxisLabelX);
-      // Set the domain for the x-axis
-      xLinearScale.domain([xMin, xMax]);
-      // Create a transition effect for the x-axis
+      x = findMinAndMax(currentAxisLabelX);
+      xLinearScale.domain([x.min, x.max]);
+
       svg
         .select(".x-axis")
         .transition()
-        // .ease(d3.easeElastic)
         .duration(1800)
         .call(bottomAxis);
-      // Select all circles to create a transition effect, then relocate its horizontal location
-      // based on the new axis that was selected/clicked
+
       d3.selectAll("circle").each(function() {
         d3
           .select(this)
           .transition()
-          // .ease(d3.easeBounce)
           .attr("cx", function(data) {
             return xLinearScale(+data[currentAxisLabelX]);
           })
           .duration(1800);
       });
 
-      // Change the status of the axes. See above for more info on this function.
-      labelChange(clickedSelection);
+      d3.selectAll(".state-abbr")
+          .transition()
+          .attr("x", function(data) {
+            return xLinearScale(+data[currentAxisLabelX]);
+          })
+          .duration(1800);
+
+      XlabelChange(clickedSelection);
+    }
+  });
+
+  d3.selectAll(".y-axis-text").on("click", function() {
+    var clickedSelection = d3.select(this);
+    var isClickedSelectionInactive = clickedSelection.classed("inactive");
+    var clickedAxis = clickedSelection.attr("data-axis-name");
+
+    if (isClickedSelectionInactive) {
+      currentAxisLabelY = clickedAxis;
+      y = findMinAndMax(currentAxisLabelY);
+      yLinearScale.domain([y.min, y.max]);
+
+      svg
+        .select(".y-axis")
+        .transition()
+        .duration(1800)
+        .call(leftAxis);
+
+      d3.selectAll("circle").each(function() {
+        d3
+          .select(this)
+          .transition()
+          .attr("cy", function(data) {
+            return yLinearScale(+data[currentAxisLabelY]);
+          })
+          .duration(1800);
+      });
+
+      d3.selectAll(".state-abbr")
+          .transition()
+          .attr("y", function(data) {
+            return yLinearScale(+data[currentAxisLabelY]);
+          })
+          .duration(1800);
+
+      YlabelChange(clickedSelection);
     }
   });
 });
